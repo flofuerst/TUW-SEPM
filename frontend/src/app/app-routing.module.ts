5981419ab6c6260a5532fe3fa1db685279da382d
12122096 <flo.fuerst@outlook.de>
2023-03-27 18:42:58 +0200

    [TS24] update routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -15,8 +15,8 @@ const routes: Routes = [
     path: 'horses', children: [
       {path: '', component: HorseComponent},
       {path: 'create', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.create}},
-      {path: 'edit/:id', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.edit}},
-      {path: 'detailView/:id', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.detailView}},
+      {path: ':id/edit', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.edit}},
+      {path: ':id/detailView', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.detailView}},
       {path: ':id/familytree', component: HorseFamilyTreeComponent},
 
     ]
